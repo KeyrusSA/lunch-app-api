ebcecbf6948b7a11fc59ee6276a2d5d8a7da95b9
Luke Silver <luke.silver@keyrus.co.za>
2025-01-19 12:55:56 +0200

    Connected to the database. Applied folder structure and installed relevant nuget packages and dependencies.

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<WeatherForecast>();
+    }
+
+
+
+}
---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+
+        return services;
+    }
+}
